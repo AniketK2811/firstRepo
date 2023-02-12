f65acb119c08fe8f055cadfea28ebd7947a2f90b
AniketK2811 <aniketlurve2811@gmail.com>
2023-02-12 13:37:46 +0530

    added

--- AJAX.java
+++ AJAX.java
@@ -3,10 +3,7 @@ public class AJAX{
 public static void main (String[]args){
 
 System.out.println("Wlcome");
-<<<<<<< HEAD
-System.out.println("12");
-=======
->>>>>>> parent of 8e1e68a (added)
+
 
 }
 
